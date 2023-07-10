7321e910406939c9d8723fe2e66df2ab5ab6c930
Kaushik Chemburkar <attherate@gmail.com>
2023-07-10 10:08:41 +1000

    Incorrect responses saved with filename with test date and time

--- main.py
+++ main.py
@@ -1,54 +1,84 @@
 import random
 import time
+from datetime import datetime
 
-def rand_num_gen(used_numbers):
-    question_num = random.randint(1, 900)
-    while question_num in used_numbers:
+class Quiz:
+    def __init__(self, filename):
+        self.filename = filename
+        self.total_questions = 0
+        self.correct_answers = 0
+        self.used_numbers = set()
+        self.incorrect_responses = []
+        self.start_time = time.time()
+
+    def rand_num_gen(self):
         question_num = random.randint(1, 900)
-    return question_num
+        while question_num in self.used_numbers:
+            question_num = random.randint(1, 900)
+        return question_num
 
-if __name__ == '__main__':
-    total_questions = 0
-    correct_answers = 0
-    start_time = time.time()
-    used_numbers = []
+    def ask_question(self, question_num):
+        with open(self.filename, "r") as file:
+            lines = file.readlines()
 
-    keep_going = True
-    incorrect_responses = []
-    while keep_going:
-        question_from_text = "QUESTION " + str(rand_num_gen(used_numbers))
-        used_numbers.append(int(question_from_text.split(" ")[1]))
-        with open("aws_test.txt", "r") as aws_test_file:
-            searchlines = aws_test_file.readlines()
-        for i, line in enumerate(searchlines):
-            if str(question_from_text) in line:
+        for i, line in enumerate(lines):
+            if f"QUESTION {question_num}" in line:
                 question_index = i
                 print("\n" + line)
-                for j in range(question_index+1, len(searchlines)):
-                    if "Correct Answer" in searchlines[j]:
-                        correct_answer = searchlines[j].split(":")[1].strip()
-                        break
-                    print(searchlines[j].strip())
-                total_questions += 1
-                question_start_time = time.time()
-                user_answer = input("Enter your answer: ")
-                question_elapsed_time = time.time() - question_start_time
 
-                if user_answer.strip() == correct_answer:
-                    correct_answers += 1
+                for j in range(question_index+1, len(lines)):
+                    if "Correct Answer" in lines[j]:
+                        correct_answer = lines[j].split(":")[1].strip()
+                        break
+                    print(lines[j].strip())
+
+                user_answer = input("Enter your answer: ").strip()
+
+                if user_answer == correct_answer:
+                    self.correct_answers += 1
                     print("Correct!")
                 else:
                     print(f"Incorrect! The correct answer is {correct_answer}")
-                    incorrect_responses.append(question_from_text + " - Incorrect answer: " + user_answer)
-                break
-        score = correct_answers/total_questions * 100
-        elapsed_time = time.time() - start_time
-        print(f"Score: {correct_answers}/{total_questions} ({score:.2f}%)")
-        print(f"Time taken for this question: {question_elapsed_time // 60:.0f} minutes and {question_elapsed_time % 60:.0f} seconds")
-        print(f"Total time taken: {elapsed_time//3600:.0f} hours and {(elapsed_time%3600)//60:.0f} minutes")
-        keep_going = input("\n" + "Do you want to continue? (y/n) ") == 'y'
+                    self.incorrect_responses.append(f"QUESTION {question_num} - Incorrect answer: {user_answer}")
 
-    if incorrect_responses:
-        with open("incorrect_responses.txt", "w") as f:
-            for response in incorrect_responses:
-                f.write(response + "\n")
+                self.total_questions += 1
+                break
+
+    def print_scores(self):
+        score = self.correct_answers / self.total_questions * 100
+        elapsed_time = time.time() - self.start_time
+        print(f"Score: {self.correct_answers}/{self.total_questions} ({score:.2f}%)")
+        print(f"Total time taken: {elapsed_time//3600:.0f} hours and {(elapsed_time%3600)//60:.0f} minutes")
+
+    def save_incorrect_responses(self):
+        if self.incorrect_responses:
+            # Get the current date and time
+            now = datetime.now()
+
+            # Format it as a string
+            date_time_str = now.strftime("%Y%m%d_%H%M%S")
+
+            # Use it in the filename
+            filename = f"incorrect_responses_{date_time_str}.txt"
+
+            with open(filename, "w") as file:
+                for response in self.incorrect_responses:
+                    file.write(response + "\n")
+
+    def run(self):
+        keep_going = True
+        while keep_going:
+            question_num = self.rand_num_gen()
+            self.used_numbers.add(question_num)
+
+            self.ask_question(question_num)
+            self.print_scores()
+
+            keep_going = input("\nDo you want to continue? (y/n) ") == 'y'
+
+        self.save_incorrect_responses()
+
+
+if __name__ == '__main__':
+    quiz = Quiz("aws_test.txt")
+    quiz.run()
